97b6737a45278e1a226ecca617f69c818c46942f
Maria Agustina Göttert <agottert@gmail.com>
2017-11-02 11:15:58 -0300

    map

---
+++ map.js
@@ -0,0 +1,40 @@
+//METODO MAP
+
+var numeros=[];//multiplicar cada uno de los eementos de numero *2
+
+/*
+console.log("Array original:"+ numeros);//me muestra el array
+console.log(" Array * 2:"+ numerosMultiplicados);//me muestra el resultado de la cuenta
+/*la funcion puede no recibir parametros,
+ son los mismo que en el forEach. */
+
+
+//esto es para hacer un array de 1000 y
+// luego a cada uno sumarle 10. 
+
+
+for (var i= 0; i<=1000; i++) {
+	numeros[i]=i;
+};
+
+console.log(numeros);
+
+var numerosMultiplicados= numeros.map(function(numero){
+
+	return numero +10; 
+});
+	
+console.log(numerosMultiplicados);
+
+
+
+
+var upper=["dulce de leche", "frutilla", "sambayon", "chocolate"];
+
+var nuevoupper= upper.map(function(e){
+
+	 return e.toUpperCase();
+	 
+});
+console.log(nuevoupper);
+
